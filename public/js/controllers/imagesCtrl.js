55975dc957e1410afd0058ce77f2c9bd7187654e
Justin Bryant <username@users.noreply.github.com'user.namegit config --global user.namegit config --global user.namegit config --global user.email username@users.noreply.github.comuser.namegit config --global user.namegit config --global user.namegit config --global user.email "username@users.noreply.github.com>
2017-02-28 22:39:03 -0700

    removed old styling in styles. created an image controller to utilize ng-repeat in carousel

---
+++ public/js/controllers/imagesCtrl.js
@@ -0,0 +1,44 @@
+(function() {
+
+  angular
+    .module('artistApp', ['slick'])
+    .controller('imageCtrl', imageCtrl, function($scope){
+        $scope.images = 
+        [ 
+            { 
+                commissions: 
+                [
+                "../Images/Commissions/by_sicilianvalkyrie:1.jpg",
+                "../Images/Commissions/by_sicilianvalkyrie:2.jpg",
+                "../Images/Commissions/by_sicilianvalkyrie:3.jpg",
+                "../Images/Commissions/by_sicilianvalkyrie:4.jpg",
+                "../Images/Commissions/by_sicilianvalkyrie:5.jpg",
+                ],
+                personal_art: 
+                [
+                "../Images/Personal-Art/by_icilianvalkyrie:1.jpg",
+                "../Images/Personal-Art/by_icilianvalkyrie:2.jpg",
+                "../Images/Personal-Art/by_icilianvalkyrie:3.jpg",
+                "../Images/Personal-Art/by_icilianvalkyrie:4.jpg",
+                "../Images/Personal-Art/by_icilianvalkyrie:5.jpg",
+                "../Images/Personal-Art/by_icilianvalkyrie:6.jpg",
+                "../Images/Personal-Art/by_icilianvalkyrie:7.jpg",
+                "../Images/Personal-Art/by_icilianvalkyrie:8.jpg",
+                "../Images/Personal-Art/by_icilianvalkyrie:9.jpg",
+                "../Images/Personal-Art/by_icilianvalkyrie:10.jpg",
+                "../Images/Personal-Art/by_icilianvalkyrie:11.jpg",
+                "../Images/Personal-Art/by_icilianvalkyrie:12.jpg",
+                "../Images/Personal-Art/by_icilianvalkyrie:13.jpg",
+                "../Images/Personal-Art/by_icilianvalkyrie:14.jpg",
+                "../Images/Personal-Art/by_icilianvalkyrie:15.jpg",
+                ]
+            }
+
+        ]
+    });
+
+    function imageCtrl () {
+      console.log('Home controller is running');
+    }
+
+})();
