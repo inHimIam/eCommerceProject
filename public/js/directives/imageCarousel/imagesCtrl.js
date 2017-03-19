1f104a228c663a567ea050c0781637b1fca551c3
Justin Bryant <username@users.noreply.github.com'user.namegit config --global user.namegit config --global user.namegit config --global user.email username@users.noreply.github.comuser.namegit config --global user.namegit config --global user.namegit config --global user.email "username@users.noreply.github.com>
2017-03-18 18:28:19 -0600

    changes were applied to try to get carousel going

--- public/js/directives/imageCarousel/imagesCtrl.js
+++ public/js/directives/imageCarousel/imagesCtrl.js
@@ -2,43 +2,41 @@
 
   angular
     .module('artistApp')
-    .controller('imagesCtrl', imagesCtrl, function($scope){
-        $scope.images = 
+    .controller('imagesCtrl', imagesCtrl);
+    function imagesCtrl($scope) {
+        console.log('images ctrl is up')
+     $scope.images = 
         [ 
             { 
                 commissions: 
                 [
-                "../Images/Commissions/by_sicilianvalkyrie:1.jpg",
-                "../Images/Commissions/by_sicilianvalkyrie:2.jpg",
-                "../Images/Commissions/by_sicilianvalkyrie:3.jpg",
-                "../Images/Commissions/by_sicilianvalkyrie:4.jpg",
-                "../Images/Commissions/by_sicilianvalkyrie:5.jpg",
+                    {url: '../Images/Commissions/by_sicilianvalkyrie:1.jpg'},
+                    {url: '../Images/Commissions/by_sicilianvalkyrie:2.jpg'},
+                    {url: '../Images/Commissions/by_sicilianvalkyrie:3.jpg'},
+                    {url: '../Images/Commissions/by_sicilianvalkyrie:4.jpg'},
+                    {url: '../Images/Commissions/by_sicilianvalkyrie:5.jpg'},
                 ],
                 personal_art: 
                 [
-                "../Images/Personal-Art/by_icilianvalkyrie:1.jpg",
-                "../Images/Personal-Art/by_icilianvalkyrie:2.jpg",
-                "../Images/Personal-Art/by_icilianvalkyrie:3.jpg",
-                "../Images/Personal-Art/by_icilianvalkyrie:4.jpg",
-                "../Images/Personal-Art/by_icilianvalkyrie:5.jpg",
-                "../Images/Personal-Art/by_icilianvalkyrie:6.jpg",
-                "../Images/Personal-Art/by_icilianvalkyrie:7.jpg",
-                "../Images/Personal-Art/by_icilianvalkyrie:8.jpg",
-                "../Images/Personal-Art/by_icilianvalkyrie:9.jpg",
-                "../Images/Personal-Art/by_icilianvalkyrie:10.jpg",
-                "../Images/Personal-Art/by_icilianvalkyrie:11.jpg",
-                "../Images/Personal-Art/by_icilianvalkyrie:12.jpg",
-                "../Images/Personal-Art/by_icilianvalkyrie:13.jpg",
-                "../Images/Personal-Art/by_icilianvalkyrie:14.jpg",
-                "../Images/Personal-Art/by_icilianvalkyrie:15.jpg",
+                    {url: '../Images/Personal-Art/by_icilianvalkyrie:1.jpg'},
+                    {url: '../Images/Personal-Art/by_icilianvalkyrie:2.jpg'},
+                    {url: '../Images/Personal-Art/by_icilianvalkyrie:3.jpg'},
+                    {url: '../Images/Personal-Art/by_icilianvalkyrie:4.jpg'},
+                    {url: '../Images/Personal-Art/by_icilianvalkyrie:5.jpg'},
+                    {url: '../Images/Personal-Art/by_icilianvalkyrie:6.jpg'},
+                    {url: '../Images/Personal-Art/by_icilianvalkyrie:7.jpg'},
+                    {url: '../Images/Personal-Art/by_icilianvalkyrie:8.jpg'},
+                    {url: '../Images/Personal-Art/by_icilianvalkyrie:9.jpg'},
+                    {url: '../Images/Personal-Art/by_icilianvalkyrie:10.jpg'},
+                    {url: '../Images/Personal-Art/by_icilianvalkyrie:11.jpg'},
+                    {url: '../Images/Personal-Art/by_icilianvalkyrie:12.jpg'},
+                    {url: '../Images/Personal-Art/by_icilianvalkyrie:13.jpg'},
+                    {url: '../Images/Personal-Art/by_icilianvalkyrie:14.jpg'},
+                    {url: '../Images/Personal-Art/by_icilianvalkyrie:15.jpg'},
                 ]
             }
 
         ]
-    });
-
-    function imagesCtrl () {
-      console.log('imageCtrl controller is running');
-    }
+    };
 
 })();
