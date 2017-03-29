1845fd13f3c66842395bb656c217a3b65d0ec875
Justin Bryant <username@users.noreply.github.com'user.namegit config --global user.namegit config --global user.namegit config --global user.email username@users.noreply.github.comuser.namegit config --global user.namegit config --global user.namegit config --global user.email "username@users.noreply.github.com>
2017-03-29 12:06:54 -0600

    changes made to imagesTmpl organization

--- public/js/directives/imageCarousel/imagesCtrl.js
+++ public/js/directives/imageCarousel/imagesCtrl.js
@@ -7,7 +7,8 @@
         console.log('images ctrl is up')
      $scope.images = 
         [ 
-            { 
+            {
+                // url: '../Images/Commissions/by_sicilianvalkyrie:1.jpg', 
                 commissions: 
                 [
                     {url: '../Images/Commissions/by_sicilianvalkyrie:1.jpg'},
@@ -18,21 +19,21 @@
                 ],
                 personal_art: 
                 [
-                    {url: '../Images/Personal-Art/by_icilianvalkyrie:1.jpg'},
-                    {url: '../Images/Personal-Art/by_icilianvalkyrie:2.jpg'},
-                    {url: '../Images/Personal-Art/by_icilianvalkyrie:3.jpg'},
-                    {url: '../Images/Personal-Art/by_icilianvalkyrie:4.jpg'},
-                    {url: '../Images/Personal-Art/by_icilianvalkyrie:5.jpg'},
-                    {url: '../Images/Personal-Art/by_icilianvalkyrie:6.jpg'},
-                    {url: '../Images/Personal-Art/by_icilianvalkyrie:7.jpg'},
-                    {url: '../Images/Personal-Art/by_icilianvalkyrie:8.jpg'},
-                    {url: '../Images/Personal-Art/by_icilianvalkyrie:9.jpg'},
-                    {url: '../Images/Personal-Art/by_icilianvalkyrie:10.jpg'},
-                    {url: '../Images/Personal-Art/by_icilianvalkyrie:11.jpg'},
-                    {url: '../Images/Personal-Art/by_icilianvalkyrie:12.jpg'},
-                    {url: '../Images/Personal-Art/by_icilianvalkyrie:13.jpg'},
-                    {url: '../Images/Personal-Art/by_icilianvalkyrie:14.jpg'},
-                    {url: '../Images/Personal-Art/by_icilianvalkyrie:15.jpg'},
+                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:1.jpg'},
+                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:2.jpg'},
+                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:3.jpg'},
+                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:4.jpg'},
+                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:5.jpg'},
+                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:6.jpg'},
+                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:7.jpg'},
+                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:8.jpg'},
+                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:9.jpg'},
+                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:10.jpg'},
+                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:11.jpg'},
+                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:12.jpg'},
+                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:13.jpg'},
+                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:14.jpg'},
+                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:15.jpg'},
                 ]
             }
 
