bc14b02630c5eb651ca1f4860b559ee90e2c3423
Justin Bryant <username@users.noreply.github.com'user.namegit config --global user.namegit config --global user.namegit config --global user.email username@users.noreply.github.comuser.namegit config --global user.namegit config --global user.namegit config --global user.email "username@users.noreply.github.com>
2017-04-08 14:56:54 -0600

    made several cosmetic adjustments removed buttons instead each art-type will have it's own carousel feature spot on the page. Finally got the images to appear from imagesCtrl now need to figure out how to make carousel work

--- public/js/directives/imageCarousel/imagesCtrl.js
+++ public/js/directives/imageCarousel/imagesCtrl.js
@@ -4,40 +4,31 @@
     .module('artistApp')
     .controller('imagesCtrl', imagesCtrl);
     function imagesCtrl($scope) {
-        console.log('images ctrl is up')
-     $scope.images = 
-        [ 
-            {
-                // url: '../Images/Commissions/by_sicilianvalkyrie:1.jpg', 
-                commissions: 
-                [
-                    {url: '../Images/Commissions/by_sicilianvalkyrie:1.jpg'},
-                    {url: '../Images/Commissions/by_sicilianvalkyrie:2.jpg'},
-                    {url: '../Images/Commissions/by_sicilianvalkyrie:3.jpg'},
-                    {url: '../Images/Commissions/by_sicilianvalkyrie:4.jpg'},
-                    {url: '../Images/Commissions/by_sicilianvalkyrie:5.jpg'},
-                ],
-                personal_art: 
-                [
-                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:1.jpg'},
-                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:2.jpg'},
-                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:3.jpg'},
-                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:4.jpg'},
-                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:5.jpg'},
-                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:6.jpg'},
-                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:7.jpg'},
-                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:8.jpg'},
-                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:9.jpg'},
-                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:10.jpg'},
-                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:11.jpg'},
-                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:12.jpg'},
-                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:13.jpg'},
-                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:14.jpg'},
-                    {url: '../Images/Personal-Art/by_sicilianvalkyrie:15.jpg'},
-                ]
-            }
-
+    $scope.personal_art =
+        [
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:1.jpg', desc: "broken image is a bookmark"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:2.jpg', desc: "granny"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:3.jpg', desc: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:4.jpg', desc: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:5.jpg', desc: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:6.jpg', desc: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:7.jpg', desc: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:8.jpg', desc: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:9.jpg', desc: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:10.jpg', desc: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:11.jpg', desc: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:12.jpg', desc: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:13.jpg', desc: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:14.jpg', desc: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:15.jpg', desc: ""}
+        ]
+     $scope.commissions = 
+        [      
+            {url: '../Images/Commissions/by_sicilianvalkyrie:1.jpg', desc: ""},
+            {url: '../Images/Commissions/by_sicilianvalkyrie:2.jpg', desc: ""},
+            {url: '../Images/Commissions/by_sicilianvalkyrie:3.jpg', desc: ""},
+            {url: '../Images/Commissions/by_sicilianvalkyrie:4.jpg', desc: ""},
+            {url: '../Images/Commissions/by_sicilianvalkyrie:5.jpg', desc: ""}
         ]
     };
-
-})();
+    })();
