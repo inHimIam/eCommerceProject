8bb3ed4ee5c9a11c8907f03dc6e0418672bec78c
Justin Bryant <username@users.noreply.github.com'user.namegit config --global user.namegit config --global user.namegit config --global user.email username@users.noreply.github.comuser.namegit config --global user.namegit config --global user.namegit config --global user.email "username@users.noreply.github.com>
2017-04-09 14:00:54 -0600

    added more images

--- public/js/directives/imageCarousel/imagesCtrl.js
+++ public/js/directives/imageCarousel/imagesCtrl.js
@@ -6,29 +6,29 @@
     function imagesCtrl($scope) {
     $scope.personal_art =
         [
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:1.jpg', desc: "broken sorry :(", title: "bookmark"},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:2.jpg', desc: "just a little granny", title: "granny"},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:3.jpg', desc: "hi", title: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:4.jpg', desc: "", title: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:5.jpg', desc: "", title: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:6.jpg', desc: "", title: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:7.jpg', desc: "", title: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:8.jpg', desc: "", title: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:9.jpg', desc: "", title: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:10.jpg', desc: "", title: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:11.jpg', desc: "", title: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:12.jpg', desc: "", title: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:13.jpg', desc: "", title: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:14.jpg', desc: "", title: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:15.jpg', desc: "", title: ""}
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:1.jpg', desc: "broken sorry :(", title: "Night and Twilight Bookmarks"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:2.jpg', desc: "just a little granny", title: "Granny Cheesecake"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:3.jpg', desc: "", title: "The Ghost Woman"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:4.jpg', desc: "My friend Natisha", title: "Portrait of Natisha"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:5.jpg', desc: "A gift for a friend", title: "Elly vs Fangirls"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:6.jpg', desc: "From the Time of the Twins a fantasy novel in the Dragonlance series written by Margaret Weis and Tracy Hickman.", title: "Rastiln & Crysania"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:7.jpg', desc: "", title: "Druid"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:8.jpg', desc: "Oil practice", title: "Waterhouse"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:9.jpg', desc: "", title: "Dark Elf Sketch"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:10.jpg', desc: "", title: "Adventurer's Tales"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:11.jpg', desc: "", title: "The Seer"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:12.jpg', desc: "", title: "Shimon"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:13.jpg', desc: "", title: "Elfa"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:14.jpg', desc: "", title: "Dark Elf Assassin"}
         ]
      $scope.commissions = 
         [      
-            {url: '../Images/Commissions/by_sicilianvalkyrie:1.jpg', desc: "", title: ""},
-            {url: '../Images/Commissions/by_sicilianvalkyrie:2.jpg', desc: "", title: ""},
-            {url: '../Images/Commissions/by_sicilianvalkyrie:3.jpg', desc: "", title: ""},
-            {url: '../Images/Commissions/by_sicilianvalkyrie:4.jpg', desc: "", title: ""},
-            {url: '../Images/Commissions/by_sicilianvalkyrie:5.jpg', desc: "", title: ""}
+            {url: '../Images/Commissions/by_sicilianvalkyrie:1.jpg', desc: "", title: "Could you Help Me Night"},
+            {url: '../Images/Commissions/by_sicilianvalkyrie:2.jpg', desc: "", title: "Could you Help Me"},
+            {url: '../Images/Commissions/by_sicilianvalkyrie:3.jpg', desc: "", title: "The Focal Pointers"},
+            {url: '../Images/Commissions/by_sicilianvalkyrie:4.jpg', desc: "", title: "The Ragers"},
+            {url: '../Images/Commissions/by_sicilianvalkyrie:5.jpg', desc: "", title: "Warrior"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:15.jpg', desc: "", title: "Voddom"}
         ]
     $scope.showDesc = false;
     $scope.description = function() {
