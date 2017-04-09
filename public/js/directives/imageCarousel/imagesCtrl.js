71e910773911be329b7bde194231d413b3ce737c
Justin Bryant <username@users.noreply.github.com'user.namegit config --global user.namegit config --global user.namegit config --global user.email username@users.noreply.github.comuser.namegit config --global user.namegit config --global user.namegit config --global user.email "username@users.noreply.github.com>
2017-04-09 12:50:00 -0600

    implimented ng-click/show events for buttons instead... not sure yet how to seclude it to a single button click

--- public/js/directives/imageCarousel/imagesCtrl.js
+++ public/js/directives/imageCarousel/imagesCtrl.js
@@ -7,28 +7,32 @@
     $scope.personal_art =
         [
             {url: '../Images/Personal-Art/by_sicilianvalkyrie:1.jpg', desc: "broken sorry :(", title: "bookmark"},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:2.jpg', desc: "granny"},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:3.jpg', desc: "hi"},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:4.jpg', desc: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:5.jpg', desc: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:6.jpg', desc: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:7.jpg', desc: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:8.jpg', desc: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:9.jpg', desc: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:10.jpg', desc: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:11.jpg', desc: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:12.jpg', desc: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:13.jpg', desc: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:14.jpg', desc: ""},
-            {url: '../Images/Personal-Art/by_sicilianvalkyrie:15.jpg', desc: ""}
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:2.jpg', desc: "just a little granny", title: "granny"},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:3.jpg', desc: "hi", title: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:4.jpg', desc: "", title: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:5.jpg', desc: "", title: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:6.jpg', desc: "", title: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:7.jpg', desc: "", title: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:8.jpg', desc: "", title: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:9.jpg', desc: "", title: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:10.jpg', desc: "", title: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:11.jpg', desc: "", title: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:12.jpg', desc: "", title: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:13.jpg', desc: "", title: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:14.jpg', desc: "", title: ""},
+            {url: '../Images/Personal-Art/by_sicilianvalkyrie:15.jpg', desc: "", title: ""}
         ]
      $scope.commissions = 
         [      
-            {url: '../Images/Commissions/by_sicilianvalkyrie:1.jpg', desc: ""},
-            {url: '../Images/Commissions/by_sicilianvalkyrie:2.jpg', desc: ""},
-            {url: '../Images/Commissions/by_sicilianvalkyrie:3.jpg', desc: ""},
-            {url: '../Images/Commissions/by_sicilianvalkyrie:4.jpg', desc: ""},
-            {url: '../Images/Commissions/by_sicilianvalkyrie:5.jpg', desc: ""}
+            {url: '../Images/Commissions/by_sicilianvalkyrie:1.jpg', desc: "", title: ""},
+            {url: '../Images/Commissions/by_sicilianvalkyrie:2.jpg', desc: "", title: ""},
+            {url: '../Images/Commissions/by_sicilianvalkyrie:3.jpg', desc: "", title: ""},
+            {url: '../Images/Commissions/by_sicilianvalkyrie:4.jpg', desc: "", title: ""},
+            {url: '../Images/Commissions/by_sicilianvalkyrie:5.jpg', desc: "", title: ""}
         ]
+    $scope.showDesc = false;
+    $scope.description = function() {
+        $scope.showDesc = !$scope.showDesc;
     };
+    }
     })();
