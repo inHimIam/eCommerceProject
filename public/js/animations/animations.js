bc31f15fb6d62293e0674bea4a9bb8270db58ba4
Justin Bryant <username@users.noreply.github.com'user.namegit config --global user.namegit config --global user.namegit config --global user.email username@users.noreply.github.comuser.namegit config --global user.namegit config --global user.namegit config --global user.email "username@users.noreply.github.com>
2017-04-25 18:44:10 -0600

    tried making the images centered in carousel ... no avail

--- public/js/animations/animations.js
+++ public/js/animations/animations.js
@@ -1,5 +1,37 @@
 $(document).ready(function(){
         
+// $('.center').slick({
+//   centerMode: true,
+//   centerPadding: '60px',
+//   slidesToShow: 3,
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 3
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 1
+//       }
+//     }
+//   ]
+// });
+// $(".featured-images").slick({
+// 	dots: true,
+//         slidesToShow: 1,
+//         centerMode: true,
+//         centerPadding: '40px',
+// });
+
         // $('.tweet-actions').hide();
         // $('.stats').hide();
         // $('.reply').hide();
